8fd42099c68824b4ff54fa480b427bf68fae1485
Leo Fabregoule <lefabreg@z4r3p5.42lyon.fr>
2024-04-06 03:14:21 +0200

    updated handle_signals.c

--- src/signals/handle_signals.c
+++ src/signals/handle_signals.c
@@ -6,13 +6,30 @@
 /*   By: lefabreg <lefabreg@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 02:58:42 by lefabreg          #+#    #+#             */
-/*   Updated: 2024/04/06 03:01:13 by lefabreg         ###   ########lyon.fr   */
+/*   Updated: 2024/04/06 03:09:45 by lefabreg         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../"
-void handle_signals()
+#include "../../includes/minishell.h"
+
+char tab[2][10] = {"EXPORT", "exit"};
+
+void	handler(int sig, siginfo_t *siginfo, void *unused)
 {
+    (void)siginfo;
+	(void)unused;
+    
+	if (sig == SIGINT)
+    {
+        printf("process killed\n");
+        exit(1);
+    }
+}
+
+void    handle_signals(void)
+{
+    struct sigaction    catch;
+    
 	sigemptyset(&catch.sa_mask);
 	catch.sa_flags = SA_SIGINFO;
 	catch.sa_sigaction = handler;
