1956db597c284d5de7e2f859a52b73428b3329e0
Pol Antoine <pantoine@student.42lyon.fr>
2024-04-06 17:13:06 +0200

    little bug in exit lol

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pantoine <pantoine@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 02:28:18 by lefabreg          #+#    #+#             */
-/*   Updated: 2024/04/06 17:08:36 by pantoine         ###   ########.fr       */
+/*   Updated: 2024/04/06 17:12:43 by pantoine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int  main(void)
         command = readline("Super prompt > ");
 		if (!ft_strncmp(command, "\n", 1))
 			continue ;
-		if (!ft_strncmp(command, "exit", 4))
+		if (!ft_strncmp(command, "exit", 4) && ft_strlen(command) == 4)
 		{
 			write(1, "exit\n", 5);
 			free(command);
