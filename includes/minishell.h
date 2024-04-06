21af2385f1056e6d70ff3546043d50c64355e4a8
Leo Fabregoule <lefabreg@z4r3p5.42lyon.fr>
2024-04-06 03:03:54 +0200

    peut etre cette fois

---
+++ includes/minishell.h
@@ -0,0 +1,11 @@
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+//readline
+#include <readline/readline.h>
+//signal
+#include <signal.h>
+
+#endif
--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int  main(void)
-{
-    printf("Hello petit coquillage!\n");
-    printf("Hello petit coquillage! (x2)\n");
-    return (0);
-}
