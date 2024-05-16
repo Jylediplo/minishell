ed574f3fb8ee9839c867ac65869d98948e2ffd2b
Jy Jy <leofabregoule@gmail.com>
2024-05-16 17:49:49 +0200

    Create free_words.c

---
+++ src/parsing/free_words.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_words.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lefabreg <lefabreg@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 14:28:36 by lefabreg          #+#    #+#             */
+/*   Updated: 2024/05/16 16:47:21 by lefabreg         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	free_wds_delim(t_words *words, int nb)
+{
+	int	i;
+
+	i = 0;
+	while (i < nb)
+	{
+		free(words->wds_delim[i++]);
+	}
+	free(words->wds_delim[i]);
+	free(words->wds_delim);
+}
+
+void	free_words(t_words *words)
+{
+	int	i;
+
+	i = 0;
+	while (i < words->num_words)
+	{
+		free(words->words[i++]);
+	}
+	free(words->words);
+}
+void	free_temp_word(char **word)
+{
+	int	i;
+
+	i = 0;
+	while (word[i])
+	{
+		free(word[i]);
+		i++;
+	}
+	free(word[i]);
+	free(word);
+}
+
+void	free_lexer1(t_words *words, int index)
+{
+	int	i;
+
+	i = 0;
+	while (i < index)
+	{
+		if (words->lexer[i]->content)
+			free(words->lexer[i]->content);
+		free(words->lexer[i]);
+		i++;
+	}
+	free(words->lexer[i]->content);
+	free(words->lexer[i]);
+	free(words->lexer);
+}
+void	free_delim(t_delims **delim)
+{
+	int	i;
+
+	i = 0;
+	while (delim[i])
+	{
+		free(delim[i]->delim);
+		free(delim[i]);
+		i++;
+	}
+	free(delim[i]);
+	free(delim);
+}
