acfd947d1ed6a17e06e2db18fbfe2ed54e3212f4
crackman36 <polrobin.antoine@gmail.com>
2024-05-31 19:44:33 +0200

    set two env variables if no envp

--- src/builtins/export_main.c
+++ src/builtins/export_main.c
@@ -6,13 +6,27 @@
 /*   By: pantoine <pantoine@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 11:19:17 by pantoine          #+#    #+#             */
-/*   Updated: 2024/05/31 19:14:32 by pantoine         ###   ########.fr       */
+/*   Updated: 2024/05/31 19:43:27 by pantoine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include "../../includes/evars.h"
 #include "../../includes/execute.h"
+
+static int	empty_envp(t_list **head)
+{
+	char	*value;
+	t_list	*envvar;
+
+	value = ft_strdup("SHLVL=1");
+	envvar = ft_lstnew(value);
+	ft_lstadd_back(head, envvar);
+	value = ft_strdup("PWD=LOOOOL");
+	envvar = ft_lstnew(value);
+	ft_lstadd_back(head, envvar);
+	return (0);
+}
 
 t_list	*copy_env(char **envp)
 {
@@ -23,6 +37,11 @@ t_list	*copy_env(char **envp)
 
 	head = NULL;
 	i = 0;
+	if (!envp[0])
+	{
+		empty_envp(&head);
+		return (head);
+	}
 	while (envp[i])
 	{
 		value = ft_strdup(envp[i++]);
